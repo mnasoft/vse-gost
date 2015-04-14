52a5d419e4b8b2122ee4eca5bb1226eb2b62e130
Nick Matvyeyev <mnasoft@gmail.com>
2015-04-14 21:12:28 +0300

    Закомментированы конфликтные выражения

--- vse-gost.lisp
+++ vse-gost.lisp
@@ -4,3 +4,17 @@
 
 ;;; "vse-gost" goes here. Hacks and glory await!
 
+;;/home/namatv/Downloads/vsegost.com/Catalog/95/9595.shtml
+
+(defparameter *fl* nil)
+(defparameter *ll* nil)
+
+;;;;(setf *fl* (open "/home/namatv/Downloads/vsegost.com/Catalog/95/9595.shtml"))
+
+;;;;(defun read-to-string(fl)
+;;;;  (do ((str "") (ln  "")))
+;;;;  (setf *ll* (read-line *fl*)))
+
+;;;;(setf *ll* (read-line *fl*))
+;;;;(search "<a href='../../Data" *ll*)
+;;;;(read-line)
