800920c3f5636f6144117c59aa9ade820d576d8d
Nick Matvyeyev <mnasoft@gmail.com>
2021-09-29 22:36:55 +0300

    Ср 29 сен 2021 19:36:55 UTC

--- sql/copy.sql
+++ sql/copy.sql
@@ -1,1 +1,3 @@
 copy gost (local_path, designation, date, name, description, status) from '/home/namatv/out.txt';
+
+copy gost (local_path, designation, date, name, description, status) from '/var/lib/postgres/out.txt';
