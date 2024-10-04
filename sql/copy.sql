d9f607bc7d66f63b3a40fa31fe0ec3aa6867cf42
Nick Matvyeyev <mnasoft@gmail.com>
2024-10-04 12:00:04 +0300

    4 окт 2024 г.  9:00:04

--- sql/copy.sql
+++ sql/copy.sql
@@ -1,3 +1,7 @@
 copy gost (local_path, designation, date, name, description, status) from '/home/namatv/out.txt';
 
 copy gost (local_path, designation, date, name, description, status) from '/var/lib/postgres/out.txt';
+
+copy gost (local_path, designation, date, name, description, status) from 'D:\home\_namatv\PRG\msys64\home\namatv\quicklisp\local-projects\clisp\vse-gost\out.txt';
+
+copy gost (local_path, designation, date, name, description, status) from 'D:/home/_namatv/PRG/msys64/home/namatv/quicklisp/local-projects/clisp/vse-gost/out.txt';
