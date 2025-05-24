f48f8127591b9486354f1ca3ef4a99ce0c54f3a7
Mykola Matvyeyev <mnasoft@gmail.com>
2025-05-24 08:02:35 +0300

    Сб 24 мая 2025 05:02:35 UTC

--- src/walk-dir.lisp
+++ src/walk-dir.lisp
@@ -1,15 +1,3 @@
 (in-package :vse-gost)
 
-(defun walk-subdirs (dir)
-  "Возвращает список директорий начиная с директория dir.
-Пример использования
-(walk-subdirs #P\"/home/namatv/My/developer/\")
-"
-  (do ((cur-dir dir (car d-lst))
-       (d-lst (list dir))
-       (rez-lst nil))
-      ((null d-lst) rez-lst)
-    (setf rez-lst (append rez-lst (list cur-dir)  )
-  	  d-lst (cdr d-lst)
-	  d-lst (append (uiop:subdirectories cur-dir) d-lst))))
 
