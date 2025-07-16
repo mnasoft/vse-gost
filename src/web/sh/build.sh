2149ac8278159c992d64e74d78055f61d836038c
Mykola Matvyeyev <mnasoft@gmail.com>
2025-07-16 07:17:51 +0300

    2025-07-16 04:17:51 UTC

--- src/web/sh/build.sh
+++ src/web/sh/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sbcl --eval "(ql:quickload :vse-gost/web)" \
+sbcl --eval "(asdf:load-system :vse-gost/web)" \
      --eval "(sb-ext:save-lisp-and-die \"vsegost-web.exe\" :executable t :compression t)"
