e423c15b0b33822e4ad35d0c6259561aab46351c
Mykola Matvyeyev <mnasoft@gmail.com>
2026-03-06 11:11:37 +0200

    2026-03-06 11:11:36

--- src/web/sh/vse-gost-web-msys2.sh
+++ src/web/sh/vse-gost-web-msys2.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# Переходим в каталог пользователя
+cd ~
+# Останавливаем сервер баз данных PostgreSQL
+"D:/home/_namatv/PRG/msys64/ucrt64/bin/pg_ctl" -D "D:/home/_namatv/PRG/msys64/var/lib/postgres/data" -l journal.log stop
 
-"D:\home\_namatv\PRG\msys64\ucrt64\bin\pg_ctl" -D "D:\home\_namatv\PRG\msys64\var\lib\postgres\data" -l journal.log stop
+# Стартуем сервер баз данных PostgreSQL
+"D:/home/_namatv/PRG/msys64/ucrt64/bin/pg_ctl" -D "D:/home/_namatv/PRG/msys64/var/lib/postgres/data" -l journal.log start
 
-"D:\home\_namatv\PRG\msys64\ucrt64\bin\pg_ctl" -D "D:\home\_namatv\PRG\msys64\var\lib\postgres\data" -l journal.log start
-
-sbcl --eval "(asdf:load-system :vse-gost/web)" --eval "(vse-gost/web:start-gosts)"
+# Старый вариант запуска
+#sbcl --eval "(asdf:load-system :vse-gost/web)" --eval "(vse-gost/web:start-gosts)"
+# Новый вариант запуска
+vsegost-web.sh
