7b3d81b4b96850783791df490312c1ff93d852a3
Mykola Matvyeyev <mnasoft@gmail.com>
2026-03-09 13:34:42 +0200

    2026-03-09 13:34:42

--- src/web/sh/build.sh
+++ src/web/sh/build.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "##################################################"
+echo "${BASH_SOURCE[0]} START ..."
+echo "##################################################"
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "${SCRIPT_DIR}/../../.." && pwd)"
-OUTPUT_PATH="${1:-${PWD}/vsegost-web.exe}"
+OUTPUT_PATH="./src/web/sh/build/vsegost-web.exe"
+
+##########
+echo "SCRIPT_DIR=${SCRIPT_DIR}"
+echo "PROJECT_ROOT=${PROJECT_ROOT}"
+echo "OUTPUT_PATH=${OUTPUT_PATH}"
+##########
 
 cd "${PROJECT_ROOT}"
+
+##########
+echo "PWD=${PWD}"
+##########
 
 sbcl --non-interactive \
      --eval "(asdf:load-system :vse-gost/web)" \
@@ -13,3 +27,7 @@ sbcl --non-interactive \
 
 # Запуск скомпилированного бинарника:
 # vsegost-web.exe --eval "(vse-gost/web:start-gosts)"
+
+echo "##################################################"
+echo "${BASH_SOURCE[0]} DONE"
+echo "##################################################"
